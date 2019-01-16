8da9cedb2dc1d4bdd417317812e8f67ff196d1ef
Kate Yeeum <yeeumm@gmail.com>
2019-01-15 16:15:16 -0800

    Setting the filtered result array to the state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Header from './components/Header';
 import WasteList from './components/WasteList';
+const LOOKUP_API_URL = 'https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000/';
 
 library.add(faSearch)
 
@@ -11,11 +13,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: ""
+      lookupData: [],
+      searchInput: "",
+      searchResult: []
     }
 
     this.searchChange = this.searchChange.bind(this);
     this.searchKeyword = this.searchKeyword.bind(this);
+    this.setSearchResult = this.setSearchResult.bind(this);
+  }
+
+  async componentWillMount() {
+    try {
+      const resp = await Axios.get(LOOKUP_API_URL);
+      const data = resp.data;
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          lookupData: data
+        }
+      });
+    } catch (err) {
+      console.log("Get Lookup data failed");
+    }
   }
 
   /**
@@ -33,7 +53,30 @@ class App extends Component {
     });
   }
 
+  /**
+   * Search keyword from the lookup data and return the filtered array
+   */
+  searchKeyword() {
+    const query = this.state.searchInput.split(' ');
+    return (query.map(q => {
+      return this.state.lookupData.filter(data => data.keywords.includes(q));
+    }));
+  }
+
+  setSearchResult() {
+    let filteredData = [];
+    filteredData = this.searchKeyword();
+
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        searchResult: filteredData
+      }
+    });
+  }
+
   render() {
+    console.log(this.state, " this is the state");
     return (
       <div className="App">
         <div className="container-fluid">
@@ -47,7 +90,7 @@ class App extends Component {
             {/* col for search input */}
 
             <div className="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-              <div className="search-button" onClick={this.searchKeyword}>
+              <div className="search-button" onClick={this.setSearchResult}>
                 <FontAwesomeIcon icon="search" className="fa-flip-horizontal" />
               </div>
               {/* .search-button */}
